11a1c06f681d1994b0a1caff6f51687cd13f6625
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2022-05-28 15:48:38 -0600

    https://www.youtube.com/watch?v=QkXeMoBPSDk

---
+++ StrongBad.md
@@ -0,0 +1,9 @@
+# Things that played while working on this.
+
+## Podcasts
+1. [Lex Fridman - Bobby Lee: Comedy, Skyrim, Sex Robots, Love, Fame, and Power](https://www.youtube.com/watch?v=tOTenjh_8hw&t=650s)
+
+## Music
+1. [I'm God - Clam's Casino, Imogen Heap](https://open.spotify.com/track/0FVuyC9RP5MACjp4lgU3qZ?si=272161bbd32f427c)
+2. [Daylight - Matt & Kim](https://open.spotify.com/track/57i3M29DWoo7RDk0Tf0LZG?si=762b85cac4204597)
+3. [Float On - Modest Mouse](https://open.spotify.com/track/2lwwrWVKdf3LR9lbbhnr6R?si=d7a8ecb7ba94400a)
--- models.py
+++ models.py
@@ -7,6 +7,13 @@ db = SqliteDatabase("dev.db")
 
 
 class BobbyLee(Model):
+    """
+    If we don't do this here, each table class (User, Message, etc)
+    will need the same
+        class Meta:
+            database = db
+    boilerplate.
+    """
     class Meta:
         database = db
 
@@ -17,8 +24,30 @@ class User(BobbyLee):
     time_stamp = DateField(default=datetime.datetime.now)
 
 
+class MessageFork(BobbyLee):
+    """
+    A fork in each message's road.
+    Is it going to a server, or is it going to a single person (direct message)?
+    Waiting on implementing this until *a* base client implementation is capable of sending messages.
+    Gives my slow silly brain time to process the super duper important decision.
+    """
+    id = UUIDField(primary_key=True, default=uuid.uuid4)
+    time_stamp = DateField(default=datetime.datetime.now)
+
+
 class Message(BobbyLee):
+    id = UUIDField(primary_key=True, defualt=uuid.uuid4)
     content = TextField()
     time_stamp = DateField(default=datetime.datetime.now)
     sent_by = ForeignKeyField(User, backref="messages")
     sent_to = ForeignKeyField(User, backref="messages")
+
+
+class Server(BobbyLee):
+    """
+    Waiting on implementing this until *a* base client implementation is capable of sending messages.
+    Gives my slow silly brain time to process the super duper important decision.
+    """
+    id = UUIDField(primary_key=True, defualt=uuid.uuid4)
+    name = TextField()
+    icon = TextField()
